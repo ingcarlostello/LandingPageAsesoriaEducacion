b32cd70b18344ffa325ebe4276a345e6a502f169
Carlos <ingcarlostello@hotmail.com>
2020-07-09 19:51:10 -0500

    se hacen retoques finales al navbar. diseño final terminado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,9 @@ import youtubeLogo from '../img/Logos/youtube-logo.png'
 import '../styles/Navbar.css';
 const Navbar = () => {
     return (
-        <nav id="navbar" className="navbar navbar-expand-lg navbar-light bg-light shadow-sm py-4 bg-white rounded">
-          
+        <nav id="navbar" className="navbar navbar-expand-lg navbar-light bg-light shadow-sm py-4 bg-white rounded">          
             <img src={youtubeLogo} alt="youtube logo" className="navbar-logo"/>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">   
                 <img className="navbar-toggler-icon" src={youtubeButton} alt="youtube"/>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
