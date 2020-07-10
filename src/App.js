fdfa50f33c2ed8046c294f1dd9e1da0e93226534
Carlos <ingcarlostello@hotmail.com>
2020-07-10 15:20:47 -0500

    se eliminan componentes que causan bug en el diseño

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import HowWorks from './components/HowWorks';
-// import Subscribe from './components/Subscribe';
+
 import PruebaSuscripcion from './components/PruebaSuscripcion';
 import Plans from './components/Plans';
-// import Subscribe2 from './components/Subscribe2';
+
 import PruebaSuscripcion2 from './components/PruebaSuscripcion2';
 import Support from './components/Support';
 import FinalSection from './components/FinalSection';
@@ -17,11 +17,9 @@ function App() {
     <div className="App">
       <Navbar />
       <Home />
-      <HowWorks />
-      {/* <Subscribe /> */}
+      <HowWorks />    
       <PruebaSuscripcion />
-      <Plans />
-      {/* <Subscribe2 /> */}
+      <Plans />      
        <PruebaSuscripcion2 />
       <Support />
       <FinalSection />
